42fff618dc0272bcc0edd805f6c881e8ba18a72c
Daria Tulickaite <jonshilin.gg@gmail.com>
2025-01-07 20:42:07 +0300

    refactor: add explicit mappings for fields

--- order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
@@ -3,13 +3,23 @@ package com.example.orderservice.orderservice.mapper;
 import com.example.orderservice.orderservice.dto.OrderDto;
 import com.example.orderservice.orderservice.entity.OrderEntity;
 
-import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false))
+
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "product", source = "product")
+    @Mapping(target = "quantity", source = "quantity")
+    @Mapping(target = "price", source = "price")
     OrderDto toDto(OrderEntity entity);
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "product", source = "product")
+    @Mapping(target = "quantity", source = "quantity")
+    @Mapping(target = "price", source = "price")
     OrderEntity toEntity(OrderDto dto);
 }
+
 

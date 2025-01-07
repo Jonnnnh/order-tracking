fbe52890fd1d1faf05ef393ca2b14867ff36d792
sinemoraa <jonshilin.gg@gmail.com>
2025-01-07 18:46:49 +0300

    refactor: enabled builder for MapStruct

--- order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
@@ -2,11 +2,14 @@ package com.example.orderservice.orderservice.mapper;
 
 import com.example.orderservice.orderservice.dto.OrderDto;
 import com.example.orderservice.orderservice.entity.OrderEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false))
 public interface OrderMapper {
     OrderDto toDto(OrderEntity entity);
+
     OrderEntity toEntity(OrderDto dto);
 }
+

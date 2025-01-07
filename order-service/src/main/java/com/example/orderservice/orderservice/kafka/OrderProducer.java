27dd9427b1cc23601e9e7c8268a66287c48a65f5
sinemoraa <jonshilin.gg@gmail.com>
2025-01-08 00:27:31 +0300

    refactor: simplified sending messages in Kafka, remove ObjectMapper

--- order-service/src/main/java/com/example/orderservice/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/kafka/OrderProducer.java
@@ -1,8 +1,7 @@
 package com.example.orderservice.orderservice.kafka;
 import com.example.orderservice.orderservice.config.KafkaTopicConfig;
 import com.example.orderservice.orderservice.dto.OrderDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -11,19 +10,12 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class OrderProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
     private final KafkaTopicConfig kafkaTopicConfig;
 
     public void sendOrderCreatedEvent(OrderDto orderDto) {
-        try {
-            String ordersTopic = kafkaTopicConfig.getOrders();
-            System.out.println("Orders topic: " + ordersTopic);
-
-            String message = objectMapper.writeValueAsString(orderDto);
-            kafkaTemplate.send(ordersTopic, message);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error of order serialization in JSON", e);
-        }
+        String ordersTopic = kafkaTopicConfig.getOrders();
+        System.out.println("Orders topic: " + ordersTopic);
+        kafkaTemplate.send(ordersTopic, orderDto);
     }
 }

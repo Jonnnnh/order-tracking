1f26c0d7a53356aab6a1e28d31ed7a7b27392366
sinemoraa <jonshilin.gg@gmail.com>
2025-01-07 18:45:32 +0300

    refactor: added @RequiredArgsConstructor and @Valid annotation to OrderController

--- order-service/src/main/java/com/example/orderservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/controller/OrderController.java
@@ -2,19 +2,23 @@ package com.example.orderservice.orderservice.controller;
 
 import com.example.orderservice.orderservice.dto.OrderDto;
 import com.example.orderservice.orderservice.service.OrderService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
+    public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody OrderDto orderDto) {
         OrderDto createdOrder = orderService.createOrder(orderDto);
         return ResponseEntity.ok(createdOrder);
     }
 }
+

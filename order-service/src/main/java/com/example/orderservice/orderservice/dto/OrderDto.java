b54f1134c6a393f7ebb27f8dc4d886f9704ed21d
sinemoraa <jonshilin.gg@gmail.com>
2025-01-08 13:09:27 +0300

    feat: add validation

--- order-service/src/main/java/com/example/orderservice/orderservice/dto/OrderDto.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/dto/OrderDto.java
@@ -2,6 +2,9 @@ package com.example.orderservice.orderservice.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Getter
@@ -11,7 +14,13 @@ import java.math.BigDecimal;
 @Builder
 public class OrderDto {
     private Long id;
+
+    @NotBlank(message = "Product name must not be empty")
     private String product;
+
+    @Min(value = 1, message = "Quantity must be greater than 0")
     private Integer quantity;
+
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
     private BigDecimal price;
 }

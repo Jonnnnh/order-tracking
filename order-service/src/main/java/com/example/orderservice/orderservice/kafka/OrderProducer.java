adba342bcad1aa7686c59424d11ad35b8fb7c806
sinemoraa <jonshilin.gg@gmail.com>
2025-01-08 13:18:05 +0300

    refactor: replace kafkaTemplate with messageProducer

--- order-service/src/main/java/com/example/orderservice/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/kafka/OrderProducer.java
@@ -3,19 +3,21 @@ import com.example.orderservice.orderservice.config.KafkaTopicConfig;
 import com.example.orderservice.orderservice.dto.OrderDto;
 
 import lombok.AllArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class OrderProducer {
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final MessageProducer<OrderDto> messageProducer;
     private final KafkaTopicConfig kafkaTopicConfig;
 
     public void sendOrderCreatedEvent(OrderDto orderDto) {
         String ordersTopic = kafkaTopicConfig.getOrders();
         System.out.println("Orders topic: " + ordersTopic);
-        kafkaTemplate.send(ordersTopic, orderDto);
+        messageProducer.sendMessage(ordersTopic, orderDto);
     }
 }

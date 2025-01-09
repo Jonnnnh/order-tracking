59f4a0517efc742c3264ae495537090b56240158
sinemoraa <jonshilin.gg@gmail.com>
2025-01-09 22:01:35 +0300

    feat: integrate MapStruct for OrderMapper implementation

--- order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/example/orderservice/orderservice/mapper/OrderMapper.java
@@ -4,16 +4,10 @@ import com.example.orderservice.orderservice.dto.OrderDto;
 import com.example.orderservice.orderservice.entity.OrderEntity;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
 
-
-//@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
-
     OrderDto toDto(OrderEntity entity);
-
     OrderEntity toEntity(OrderDto dto);
 }
 

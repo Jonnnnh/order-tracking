cd036883d1e76c839d6c32e9d29154c80cb9aa33
sinemoraa <jonshilin.gg@gmail.com>
2025-01-10 23:43:30 +0300

    feat: add for handling order creation events from Kafka

---
+++ notification-service/src/main/java/com/example/notificationservice/listener/OrderCreatedListener.java
@@ -0,0 +1,26 @@
+package com.example.notificationservice.listener;
+
+import com.example.notificationservice.dto.OrderCreatedEvent;
+import com.example.notificationservice.saga.NotificationSagaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderCreatedListener {
+
+    private final NotificationSagaService notificationSagaService;
+
+    @KafkaListener(topics = "orders", groupId = "mapper-group")
+    public void handleOrderCreated(OrderCreatedEvent event) {
+        log.info("Order creation event received: {}", event);
+        try {
+            notificationSagaService.handleOrderCreatedEvent(event);
+        } catch (Exception e) {
+            log.error("Error processing order created event: {}", event, e);
+        }
+    }
+}

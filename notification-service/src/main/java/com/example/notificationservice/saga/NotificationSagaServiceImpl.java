19339742c86c4a6438c57151e85b83c3f1d2c67c
sinemoraa <jonshilin.gg@gmail.com>
2025-01-10 23:41:40 +0300

    feat: add for handling and compensating order events

---
+++ notification-service/src/main/java/com/example/notificationservice/saga/NotificationSagaServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.notificationservice.saga;
+
+import com.example.notificationservice.dto.OrderCreatedEvent;
+import com.example.notificationservice.service.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@AllArgsConstructor
+public class NotificationSagaServiceImpl implements NotificationSagaService {
+
+    private final NotificationService notificationService;
+
+    @Override
+    @Transactional
+    public void handleOrderCreatedEvent(OrderCreatedEvent event) {
+        try {
+            log.info("Handling order created event: {}", event);
+            notificationService.processNotification(event);
+
+        } catch (Exception e) {
+            log.error("Error processing order created event. Compensating...", e);
+            compensateNotification(event);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void compensateNotification(OrderCreatedEvent event) {
+        try {
+            log.info("Compensating notification for event: {}", event);
+            notificationService.deleteNotification(event.getId());
+        } catch (Exception ex) {
+            log.error("Error during compensation for order created event: {}", event, ex);
+        }
+    }
+}
